b45b0d66a65e55dda3258d5f2f90ac571b01042e
Dominique Mattern <dominique@mattern.dev>
2022-06-30 09:15:56 +0200

    Add points multiplier column to chef model

--- serienabend_shef/db/models/chef.py
+++ serienabend_shef/db/models/chef.py
@@ -7,7 +7,8 @@ class Chef(Base):
 
     id = Column(Integer, primary_key=True)
     name = Column(String(255), nullable=False, unique=True)
-    points = Column(Integer, server_default=text("0"))
+    points = Column(Integer, server_default=text("0"), nullable=False)
+    points_multiplier = Column(Integer, server_default=text("1"), nullable=False)
 
     def __repr__(self):
         return f"Chef(id={self.id!r}, name={self.name!r}, points={self.points!r})"
